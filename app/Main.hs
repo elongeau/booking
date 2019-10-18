943849e3b3b4a92358035f1e53b4e3f270288911
Emmanuel LONGEAU <emmanuel.longeau@gmail.com>
2019-10-18 17:25:38 +0200

    feat: Get a booking

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Data.Monoid (mconcat)
+import Web.Scotty
 
 main :: IO ()
-main = someFunc
+main = scotty 3000 $
+    get "/:word" $ do
+        beam <- param "word"
+        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
